d9c47e931e352b16523ebe08b9923b33d14383c1
Xyembeu <knoppixglyuk@gmail.com>
2019-09-25 21:20:30 +0600

    use hooks
    (cherry picked from commit e5da5f749083e0e37aee85a5003aeb49615f1e11)

--- src/containers/book-list-container/book-list-container.js
+++ src/containers/book-list-container/book-list-container.js
@@ -16,6 +16,7 @@ class BookListContainer extends Component {
     componentDidMount() {
         const {booksFetch} = this.props;
         booksFetch(true);
+        console.log('999');
     }
 
 
@@ -33,6 +34,8 @@ class BookListContainer extends Component {
                 <div>{error}</div>
             );
         }
+
+        console.log(products)
 
         return (
             <>
