d76943b98d056c4ef329d5688afbb5b1ddaad23f
Xyembeu <knoppixglyuk@gmail.com>
2019-11-21 19:11:13 +0600

    use hooks
    (cherry picked from commit e5da5f749083e0e37aee85a5003aeb49615f1e11)

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,17 +1,90 @@
 import React, {Component} from 'react';
-import Constructor from "../containers/constructor-container/constructor-container";
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css'; // see installation section above for versions of NPM older than 3.0.0
+import src from './1.jpg'
 
-class HomePage extends Component {
+
+export default class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            src,
+            cropResult: null,
+            rotation: 0
+        };
+        this.cropImage = this.cropImage.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+
+    onChange(e) {
+        e.preventDefault();
+        let files;
+        if (e.dataTransfer) {
+            files = e.dataTransfer.files;
+        } else if (e.target) {
+            files = e.target.files;
+        }
+        const reader = new FileReader();
+        reader.onload = () => {
+            this.setState({src: reader.result});
+        };
+        reader.readAsDataURL(files[0]);
+    }
+
+    cropImage() {
+        if (typeof this.cropper.getCroppedCanvas() === 'undefined') {
+            return;
+        }
+        this.setState({
+            cropResult: this.cropper.getCroppedCanvas().toDataURL(),
+        });
+    }
+
+
     render() {
+        console.log(this.state.rotation)
         return (
-            <div className="container">
-                <div className="col-md-12">
-                    <h4>WELCOME</h4>
-                    <Constructor/>
+            <div>
+                <div>
+                    <input type="file" onChange={this.onChange}/>
+                    <Cropper
+                        style={{height: 300, width: 400}}
+                        aspectRatio={1}
+                        guides={false}
+                        src={this.state.src}
+                        ref={cropper => {
+                            this.cropper = cropper;
+                        }}
+                    />
                 </div>
+                <div>
+                    <div className="box">
+                        <h1>
+                            <span>Crop</span>
+                            <button onClick={this.cropImage} style={{float: 'right'}}>
+                                Crop Image
+                            </button>
+                        </h1>
+                        <img src={this.state.cropResult} alt="cropped image"/>
+                    </div>
+                </div>
+                <button onClick={() => {
+                    this.setState(({rotation}) => {
+                        return {
+                            rotation: rotation === 270 ? 0 : rotation + 90
+                        }
+                    }, () => {
+                        this.cropper.rotateTo(this.state.rotation)
+                    })
+                }
+                }>Повернуть
+                </button>
             </div>
         );
     }
 }
 
-export default HomePage;
+
+
